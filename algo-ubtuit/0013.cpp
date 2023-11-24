6117f2488c4d2bdf04118c688c538a34f57121a6
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-11-24 17:30:14 +0500

    Create 0013.cpp

---
+++ algo-ubtuit/0013.cpp
@@ -0,0 +1,11 @@
+/* m massali jismga a tezlanish berilganda unga qanchali kuch tasir qiladi. */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int m,a;
+  cin>>m>>a;
+  cout<<m*a;
+  return 0;
+}
