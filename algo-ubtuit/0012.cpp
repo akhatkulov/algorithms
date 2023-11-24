66d60d366fcc3e0370df53e2dcea2766c4d957a1
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-11-24 17:16:12 +0500

    Create 0012.cpp

---
+++ algo-ubtuit/0012.cpp
@@ -0,0 +1,14 @@
+/* Massasi m bo`lgan jismni og`irligini toping(g=9.8). */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double a;
+    double g = 9.8;
+    cin>>a;
+    cout.precision(2);
+    cout<<fixed<<a*g;
+    return 0;
+}
