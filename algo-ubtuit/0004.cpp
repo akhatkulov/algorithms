a54464de79a0870b9a4a8746f17e3c83fbf9d689
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-11-24 17:05:50 +0500

    Create 0004.cpp

---
+++ algo-ubtuit/0004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int r;
+    cin>>r;
+    cout.precision(2);
+    double res = 4*M_PI*r*r;
+    cout<<fixed<<res;
+}
