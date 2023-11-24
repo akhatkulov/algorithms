d0621f695c8d045e04913b0be8da22f1c9e55e8a
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-11-24 17:08:27 +0500

    Create 0009.cpp

---
+++ algo-ubtuit/0009.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double g = 9.8;
+    double h;
+    cin>>h;
+    cout.precision(2);
+    cout<<fixed<<sqrt(2*h/g);
+}
