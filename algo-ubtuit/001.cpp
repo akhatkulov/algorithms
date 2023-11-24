459dc86c0bc6c7d75f234cd6d67a8ee96148991c
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-11-24 17:04:21 +0500

    Create 001.cpp

---
+++ algo-ubtuit/001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int x,y,z;
+    const double PI = M_PI;
+    cin>>x>>y>>z;
+    cout.precision(2);
+    cout<<fixed<<pow(x,2)*PI<<" "<<pow(y,2)*PI<<" "<<pow(z,2)*PI;
+}
+
+#Algo  #ubtuiut #002
