13557a8360dd9028779e53d42f224ac5417ee9fe
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-11-24 17:07:38 +0500

    Update 0007.cpp

--- algo-ubtuit/0007.cpp
+++ algo-ubtuit/0007.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// V = (1/3) * π * r^2 * h
+int main(){
+    int r,h;
+    cin>>h>>r;
+    cout.precision(2);
+    cout<<fixed<<(M_PI*(r*r)*h)/3;
+}
 
