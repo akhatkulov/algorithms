1626f983c3c360cbd9d76be337d04d207eb8e2dc
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-12-13 08:18:30 +0500

    Create 0012.cpp

---
+++ robocontest/0012.cpp
@@ -0,0 +1,15 @@
+/* Ali va Bobur quyidagicha o'yin o'ynashyapti:
+- o'yin bitta butun son, n soni bilan boshlanadi, bu son 1 ,1 dan n gacha bo'lgan to'plamni tayyorlab olishda ishlatiladi. ya'ni to'plam = {1,2,…,1,}
+{1,2,…,n−1,n}
+- o'yinni Ali boshlab beradi va ular navbatma-navbat o'z yurishlarini amalga oshirishadi.
+- har bir yurishda o'yinchi to'plamda mavjud sonlardan ixtiyoriy bir tub sonni tanlab oladi va to'plamdan shu tub songa bo'linadigan barcha sonni olib tashlaydi.
+- o'zining navbati kelganida yurishni amalga oshira olmagan o'yinchi o'yinda yutqazadi.
+Sizga n soni beriladi. o'yinda kim g'olib bo'lishini aniqlang. /*
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  
+}
