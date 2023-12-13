a60714432a7ab7a96d4c0674c78f36e9a2e63cad
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2023-12-13 08:33:18 +0500

    Create (x)0014.cpp

---
+++ robocontest/(x)0014.cpp
@@ -0,0 +1,24 @@
+/* Uxonoid sayyorasida yashovchi insonlar soni cheksiz va bu insonlarning hech biri vafot etmaydi.
+
+Aynan shu sayyorada yovvoyi jonivorlarni iste'mol qilish oqibatida bir inson yuqumli virus yuqtirib oldi.
+
+Bu virus faqatgina virus bilan kasallangan inson boshqa bir sog'lom inson bilan muloqotda bo'lganda kasallangan insondan sog'lom insonga o'tadi.
+
+Uxonoid sayyorasida yashovchi insonlarning har biri bir kun mobaynida aynan K ta inson bilan ko'rishadi.
+
+Shu sayyorada bir inson jonivordan virus yuqtirib olganidan N kundan keyin sayyorada virus bilan kasallanganlar soni ko’pi bilan nechta bo'lishini aniqlang. */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  long long a,b;
+  cin>>a>>b;
+  if(a==0){
+    cout<<1;
+  }else{
+    cout<<(b+1)*(b+1);
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////XATO???????????????????????????????????????????????????????????????????????
