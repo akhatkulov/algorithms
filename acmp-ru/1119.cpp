8d95ab2b682bd03af420fb7b7c013c749e0dfb12
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-02-07 14:42:36 +0500

    Create 1119.cpp

---
+++ acmp-ru/1119.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a,b,a1,b1;
+    cin>>a>>b;
+    cin>>a1>>b1;
+    if(a-a1==0 or b-b1 == 0)
+    {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+}
