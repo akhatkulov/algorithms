236be01b6d723a391af71965cc2ee804c66ad693
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-02-07 14:43:06 +0500

    Create 1120.cpp

---
+++ acmp-ru/1120.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main()
+{
+    int a,b,a1,b1;
+    cin>>a>>b;
+    cin>>a1>>b1;
+    if(a1-a == b1-b or a1-a == -(b1-b))
+    {
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+}
