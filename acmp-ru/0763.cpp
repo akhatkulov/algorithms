2a85ce128ea57c570e9d9cf23b116ed7f3276f1c
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-02-07 14:46:25 +0500

    Create 0763.cpp

---
+++ acmp-ru/0763.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+ 
+{
+    int a,b;
+    cin>>a>>b;
+    if(a==1 and b ==1)
+    {
+        cout<<"0";
+    }
+    else if(a==b)
+    {
+        cout<<"2";
+    }
+    else
+    {
+        cout<<"1";
+    }
+     
+}
