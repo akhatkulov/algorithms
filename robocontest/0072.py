d7b73e9ebceef780e5be25ba4ad725005c282329
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-09-18 08:50:12 +0500

    Update 0072.py

--- robocontest/0072.py
+++ robocontest/0072.py
@@ -1,3 +1,12 @@
+"""
+Three points (x1,y1), (x2,y2), and (x3,y3) form a triangle if and only if they are not collinear. This can be checked using the determinant of a matrix formed by these points. Specifically, you can compute the area of the triangle using the following formula:
+ ∣x1(y2−y3)+x2(y3−y1)+x3(y1−y2)∣
+
+If the area is zero, then the points are collinear and thus do not form a triangle.
+
+"""
+
+
 from math import sqrt,floor
 
 x1,y1 = map(int,input().split())
