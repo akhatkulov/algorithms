403af92a6df9cc19d8cb3462992c7c64c8e11a73
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-09-18 08:48:53 +0500

    Create 0072.py

---
+++ robocontest/0072.py
@@ -0,0 +1,12 @@
+from math import sqrt,floor
+
+x1,y1 = map(int,input().split())
+x2,y2 = map(int,input().split())
+x3,y3 = map(int,input().split())
+
+area = abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))
+
+if area!=0:
+  print("uchburchak")
+else:
+  print("uchburchak emas")
