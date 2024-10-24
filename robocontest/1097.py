8c2d10a023cfcdebcb1db5e3119bcc286d0efc4f
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-10-24 09:54:45 +0500

    Update 1097.py

--- robocontest/1097.py
+++ robocontest/1097.py
@@ -1,15 +1,9 @@
-def max_money_from_check(N):
-    N_str = str(N)
-    max_value = N 
-    for i in range(len(N_str)):
-        original_digit = N_str[i]
-        for new_digit in '0123456789':
-            if new_digit != original_digit:
-                new_number = N_str[:i] + new_digit + N_str[i+1:]
-                if new_number[0] != '0':  
-                    max_value = max(max_value, int(new_number))
-
-    return max_value
-
-N = int(input().strip())
-print(max_money_from_check(N))
+def calc(n:int) -> str:
+    s = list(str(n))
+    for i in range(len(s)):
+        if s[i] != '9':
+            s[i] = '9'
+            break
+    return ''.join(s)
+n = int(input())
+print(calc(n))
