7c77641ba753971b6c6171909bbc765c4f76f42d
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-10-28 20:16:18 +0500

    Create fibonacci_v1.cpp

---
+++ DS_A/Dynamic_Programming/fibonacci_v1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#define ll long long
+using namespace std;
+
+vector <ll> baza;
+
+ll f(ll a){
+  if(a==0) return 0;
+  if(a==1) return 1;
+  
+  if(baza[a]!=-1) return a;
+  
+  return f(a-1) + f(a-2);
+}
+
+int main() 
+{
+    ll a;
+    cin>>a;
+    baza.resize(a+1,-1);
+    cout<<f(a);
+}
