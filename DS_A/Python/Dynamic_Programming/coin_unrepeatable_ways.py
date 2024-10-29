ee12a1f22b5529a10d7c2cfcbefad6a7b32c21cd
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-10-29 20:15:10 +0500

    Create coin_unrepeatable_ways.py

---
+++ DS_A/Python/Dynamic_Programming/coin_unrepeatable_ways.py
@@ -0,0 +1,14 @@
+mod = 1000000007 # masalada 10ning 9 darajasi + 7 ga qoldiqli bo'l degan
+
+n,x = map(int,input().split()) ## n tangalar soni, x bu qiymat
+l = list(map(int,input().split())) #tangalar
+
+baza = [0]*(x+1) #baza yaratish
+baza[0] = 1 #bazani sozlash
+
+for j in l: # tangalarni birma bir olamiz
+  for i in range(1,x+1): # 1dan qiymatgacha yuramiz
+    if(i-j>=0): #manfiy qiymat emasligini tekshiramiz
+    	baza[i]=(baza[i]+baza[i-j])%mod  # qoldiq bo'lib har usulni qo'shib ketaveramiz
+
+print(baza[x]) # qiymat usullarini chiqaramiz
