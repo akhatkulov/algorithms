333625e9a47cb25b5380c253fcccb66db1147377
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-10-29 20:11:04 +0500

    Create coin_repeatable_ways.cpp

---
+++ DS_A/C++/Dynamic_Programming/coin_repeatable_ways.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#define ll long long
+using namespace std;
+
+int main() 
+{
+    ll MOD = 1000000007;   //masalada 10ning 9 darajasi + 7 ga qoldiqli bo'l degan
+    int n,x;
+    cin>>n>>x; // n tangalar soni, x bu qiymat
+    
+    vector<int> tangalar(n);  //tangalar
+    
+    for(int i = 0; i<n; i++){
+      cin>>tangalar[i];
+    }
+    
+    
+    vector <int> baza(x,0); // baza yaratdik
+    baza[0] = 1; // bazani sozladik
+    
+    for(int i = 0; i<=x; i++){ // qiymatga qarab yurdik
+      for(int j: tangalar){ // tangalarni oldik
+        if(i-j>=0) baza[i] =  (baza[i] + baza[i-j])%MOD; // qoldiqga bo'lik undan oldin esa qiymat yo'llarini o'zaro qo'shib oldik
+      }  
+    }
+    
+    cout<<baza[x];
+    
+}
