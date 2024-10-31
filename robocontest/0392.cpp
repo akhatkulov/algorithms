92e9e065ebecc9009c558999d00e700df4632801
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-10-31 21:26:56 +0500

    Create 0392.cpp

---
+++ robocontest/0392.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+#define ll long long
+
+using namespace std;
+
+int main(){
+	ll a,b;
+	cin>>a;
+	for(int i=0 ; i<a; i++){
+		cin>>b;
+		cout<<floor(b/2)<<endl;
+	}
+}
