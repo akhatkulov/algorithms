5835bcc18c42783c3842e1cce849f676b26ad95c
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-11-25 08:50:35 +0500

    Update eratosthenes_sieve.py

--- DS_A/Python/math/eratosthenes_sieve.py
+++ DS_A/Python/math/eratosthenes_sieve.py
@@ -2,7 +2,7 @@ def eratasfen(limit):
 	is_p = [True] * (limit+1)
 	is_p[0] = is_p[1] = False
 	
-	for i in range(0,int(limit**0.5)+1):
+	for i in range(1,int(limit**0.5)+1):
 		if is_p[i]:
 			for j in range(i*i,limit+1,i):
 				is_p[j]=False
